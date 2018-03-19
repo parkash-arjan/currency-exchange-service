cc7af5eb7e77907c4f7950e5f856fe4298bb2727
parkash-arjan <parkash.arjan.github@gmail.com>
2018-03-18 21:45:41 -0400

    corrected sql

--- currency-exchange-service/src/main/java/com/euler/microservices/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/euler/microservices/currencyexchangeservice/ExchangeValue.java
@@ -2,13 +2,17 @@ package com.euler.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
-//@Entity
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class ExchangeValue {
-	// @Id
+	@Id
 	private Long id;
-	// @Column(name="currency_from")
+	@Column(name = "currency_from")
 	private String from;
-	// @Column(name = "currency_to")
+	@Column(name = "currency_to")
 	private String to;
 	private BigDecimal conversionMultiple;
 	private int port;
